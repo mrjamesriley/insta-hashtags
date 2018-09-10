f49f77a64886d71e067e600acf44bd54ca5594dc
James Riley <james@codansa.com>
2018-09-10 21:23:06 +0100

    switch to Postgres and configure for Heroku

--- insta_hashtags/settings.py
+++ insta_hashtags/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,9 +84,10 @@ WSGI_APPLICATION = 'insta_hashtags.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'instaman',
         'PASSWORD': '',
+        'HOST': 'localhost',
         'NAME': 'hashtags'
     }
 }
@@ -128,5 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# use django-heroku to set database settings when on Heroku
+django_heroku.settings(locals())
 
 
